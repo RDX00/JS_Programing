e1d28057cb1be81159c8b4adf681a91ab732d8b0
unknown <mishalrdx78@gmail.com>
2026-04-28 15:43:23 +0600

    add my JavaScript task5

---
+++ task5.js
@@ -0,0 +1,14 @@
+let num1 = 20;
+let num2 = 20;
+let resul;
+
+if(num1 > num2){
+    console.log(result = num1*2)
+}
+else{
+    console.log(result = num1 + num2)
+}
+
+// let result2 = (num1 > num2) ? num1*2 : num1 + num2 
+// console.log(result2)
+
