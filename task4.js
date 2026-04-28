a2c435876da99516bdc0a324816ce795cad20218
unknown <mishalrdx78@gmail.com>
2026-04-28 15:30:17 +0600

    add my JavaScript task4

---
+++ task4.js
@@ -0,0 +1,21 @@
+let money = 85;
+let friendMoney = 59;
+if(money > 80){
+    console.log('Inside my friend')
+    if(friendMoney > 80){
+        console.log('Then Go For A Lunch')
+    }
+    else if(friendMoney >= 60){
+        console.log('Good Luck next time')
+    }
+    else if(friendMoney >= 40){
+        console.log('Kepp your friend massage unseen')
+    }
+    else {
+        console.log('Block your friend')
+    }    
+}
+else{
+    console.log('I am a fokir')
+}
+
