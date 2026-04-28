976133c93fafb8a22f361e0808fb56b5727528fd
unknown <mishalrdx78@gmail.com>
2026-04-28 15:04:19 +0600

    add my JavaScript task3

---
+++ task3.js
@@ -0,0 +1,19 @@
+let score = 75;
+if(score >= 90 && score <= 100 ){
+    console.log('A')
+}
+else if(score >= 80){
+    console.log('B')
+}
+else if(score >= 70){
+    console.log('C')
+}
+else if(score >= 60){
+    console.log('D')
+}
+else if(score >= 0 && score <= 59){
+    console.log('F')
+}
+else{
+    console.log('Invalid Result')
+}
