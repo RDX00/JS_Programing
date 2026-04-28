55f4c33c2cb89cf75abd296b5d9c2db7048221e4
unknown <mishalrdx78@gmail.com>
2026-04-28 23:11:28 +0600

    add my JavaScript task6

---
+++ task6.js
@@ -0,0 +1,10 @@
+let age = 5;
+if(age < 10){
+    console.log('Student get 10% Discount')
+}
+else if( age >= 60 ){
+    console.log('Get a 15% Discout')
+}
+else{
+    console.log('ticket price fare 800')
+}
